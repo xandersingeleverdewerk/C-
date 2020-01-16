88848821f1268803f250a3c8be008e74f8d80c70
xandersingeleverdewerk <47180650+xandersingeleverdewerk@users.noreply.github.com>
2020-01-16 22:46:18 +0100

    Add files via upload

---
+++ Welkom/Welkom/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Welkom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            String naam = Environment.UserName;
+            textBox1.Text = "Beste (" + naam + "), \r\n Welkom in ons systeem. Zoals je zult begrijpen zijn er enkele regels op ons \r\n systeem van toepassing waar iedere gebruiker zich aan moet houden. Door op de \r\n knop 'ok' te klikken gaat  ermee akkoord dat de systeembeheerde onbeperkt \r\n toegang heeft tot uw computer";
+        }
+    }
+}
---
+++ wpfTest/wpfTest/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace wpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbxNaam.Text = "naam";
+            tbxStraat.Text = "adres";
+        }
+
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Text = "";
+        }
+
+        private void TbxNaam_LostFocus(object sender, RoutedEventArgs e)
+        {
+            tbxNaam.Text = tbxNaam.Text.ToUpper();
+        }
+
+        private void TbxStraat_LostFocus(object sender, RoutedEventArgs e)
+        {
+            tbxStraat.Text = tbxStraat.Text.ToUpper();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+    }
+}
